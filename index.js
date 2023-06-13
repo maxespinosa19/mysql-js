aba44b34055d61371c4a8cf1969c5f3cb9d2d09f
Max Espinosa <maxespinosa@Maxs-MacBook-Air.local>
2023-06-13 12:13:06 -0400

    created

---
+++ index.js
@@ -0,0 +1,27 @@
+// import the mysql2 library
+import mysql from 'mysql2';
+import connectionInfo from './secretstuff.js';
+
+
+const connection = mysql.createConnection(connectionInfo);
+
+//-----ONE WAY------------
+
+// const myQuery = 'SELECT * FROM movies'
+// function handleResults(error, results) {
+//     console.log(error)
+//     console.table(results)
+// }
+//  connection.query(myQuery, handleResults)
+// connect to OUR mysql databse (give it OUR crednetials)
+// -------ANOTHER WAY------
+
+// run a simple query to get 2 movies
+connection.query(
+    'SELECT * FROM movies',
+    function (error , results) {
+    console.log(error)
+    // console.table the results
+    console.table(results)
+  }
+)
